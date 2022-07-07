0cfa5278a6cc6894b2979ef23bb032d0b55a1789
sieroniekuggy <sieroniekuggy@gmail.com>
2022-07-07 13:54:56 +0200

    migration commands

--- database/migrations/2022_07_07_114727_create_sie_table.php
+++ database/migrations/2022_07_07_114727_create_sie_table.php
@@ -13,11 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('test', function (Blueprint $table) {
+        Schema::create('sie', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('address');
-            $table->text('comment');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('test');
+        Schema::dropIfExists('sie');
     }
 };
