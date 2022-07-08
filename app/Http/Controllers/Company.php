3d01e1e6181ef7976a0758d25ee39da60d6aa6fe
sieroniekuggy <sieroniekuggy@gmail.com>
2022-07-08 22:58:57 +0200

    Stubs

---
+++ app/Http/Controllers/Company.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Company extends Controller
+{
+    //
+    function index()
+    {
+        return 'Hello from index stub';
+    }
+}
